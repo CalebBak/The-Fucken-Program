a2ca2fd31505b73fa4922bed8bf43845760f00ef
novo52 <littleboboonska@gmail.com>
2020-04-03 14:56:27 -0600

    added more words

--- src/words.hpp
+++ src/words.hpp
@@ -13,6 +13,27 @@ const std::string DETERMINERS[TOTAL_DETERMINERS] = {
 		"the", "their", "these", "this", "those", "very", "whose", "your"
 };
 
+#define TOTAL_BASIC_EXPLICATIVES 5
+const std::string BASIC_EXPLICATIVES[TOTAL_BASIC_EXPLICATIVES] = {
+		"damn", "goddamn", "fucking", "fuckin\'", "bitch-ass"
+};
+
+#define TOTAL_LEVELS 4
+const std::string LEVELS[TOTAL_LEVELS] = {
+		"absolutely", "literally", "really", "incredibly"
+};
+
+#define TOTAL_ENDINGS 6
+const std::string ENDINGS[TOTAL_ENDINGS] = {
+		"garbage", "bad", "terrible", "ungood", "jank", "the worst"
+};
+
+#define TOTAL_MODIFIED 6
+const std::string MODIFIED[TOTAL_MODIFIED] = {
+		"jank", "tall", "short", "long", "old", "strong", "poor", "huge", "rare", "pure"
+};
+
+
 /*
  * Add expletives into the string after determiners. Modifies the given string.
  *
